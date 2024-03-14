7768e8cc698d4b56b9dcf02bf1e04d9d55eb1eb4
Roman Bondarev <Rbondarev22@gmail.com>
2024-03-15 00:41:34 +0700

    add constans

--- src/components/PaymentMethod/PaymentCards/index.tsx
+++ src/components/PaymentMethod/PaymentCards/index.tsx
@@ -1,34 +1,35 @@
 import Card from "./Card";
-import payToHire from "../../../assets/icons/super-boss.svg";
-import fiftyFifty from "../../../assets/icons/man-working.svg";
-import payAfterMonth from "../../../assets/icons/man-writing.svg";
-
-const titlePayToHire = "100% за выход сотрудника";
-const textPayToHire =
-  "Если хотите, чтобы на заявку откликались «звёздные» рекрутеры с опытом, выбирайте этот вариант оплаты. Чтобы ещё больше повысить шансы на отклик от такого исполнителя, предлагайте оплату выше среднемесячного оклада нанимаемого кандидата";
-const titleFiftyFifty = "50% — после выхода, 50% — после гарантийного периода";
-const textFiftyFifty =
-  "Гарантийный период — испытательный срок, который определяете вы. Обычно это 1 месяц после выхода сотрудника";
-const titlePayAfterMonth = "100% после гарантийного периода";
-const textPayAfterMonth =
-  "За такие заявки берутся реже всего — для рекрутера это риск не получить оплату, если компани и сотрудник прекратят сотрудничество";
+import payToHireImage from "../../../assets/icons/super-boss.svg";
+import fiftyFiftyImage from "../../../assets/icons/man-working.svg";
+import payAfterMonthImage from "../../../assets/icons/man-writing.svg";
+import { CARDS_TEXT } from "../../../utils/constans";
 
 function CardPayToHire() {
-  return <Card img={payToHire} title={titlePayToHire} text={textPayToHire} />;
+  return (
+    <Card
+      img={payToHireImage}
+      title={CARDS_TEXT.PAY_TO_HIRE.TITLE}
+      text={CARDS_TEXT.PAY_TO_HIRE.TEXT}
+    />
+  );
 }
 
 function CardFiftyFifty() {
   return (
-    <Card img={fiftyFifty} title={titleFiftyFifty} text={textFiftyFifty} />
+    <Card
+      img={fiftyFiftyImage}
+      title={CARDS_TEXT.FIFTY_FIFTY.TITLE}
+      text={CARDS_TEXT.FIFTY_FIFTY.TEXT}
+    />
   );
 }
 
 function CardPayAfterMonth() {
   return (
     <Card
-      img={payAfterMonth}
-      title={titlePayAfterMonth}
-      text={textPayAfterMonth}
+      img={payAfterMonthImage}
+      title={CARDS_TEXT.PAY_AFTER_MONTH.TITLE}
+      text={CARDS_TEXT.PAY_AFTER_MONTH.TEXT}
     />
   );
 }
