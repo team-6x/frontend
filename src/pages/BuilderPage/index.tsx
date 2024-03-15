2325224a23ea62dc45dcff49efea1be55f1ba000
Constantine <asbest911@gmail.com>
2024-03-15 18:00:52 +0300

    feat: add button ui

--- src/pages/BuilderPage/index.tsx
+++ src/pages/BuilderPage/index.tsx
@@ -1,4 +1,5 @@
 import PaymentMethod from '../../components/PaymentMethod';
+import Button from '../../ui-kit/Button';
 import CustomText from '../../ui-kit/CustomText';
 import Title from '../../ui-kit/Title';
 
@@ -8,6 +9,14 @@ export default function BuilderPage() {
       <h1>Builder</h1>
       <CustomText text="CustomText!" weight="bold" size="20px" />
       <Title text="Title!" tag="h2" />
+      <Button view="flat">Кнопка </Button>
+      <Button view="filled">Кнопка </Button>
+      <Button view="flat" variant="secondary">
+        Кнопка
+      </Button>
+      <Button view="filled" variant="secondary">
+        Кнопка
+      </Button>
       <PaymentMethod />
     </>
   );
