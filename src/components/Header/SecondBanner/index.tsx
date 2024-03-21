0de46c5253f8b0c4f90ec6b7a8cbc63127050beb
Constantine <asbest911@gmail.com>
2024-03-21 18:49:20 +0300

    fix: fix styles

--- src/components/Header/SecondBanner/index.tsx
+++ src/components/Header/SecondBanner/index.tsx
@@ -3,39 +3,41 @@ import { Text, StarIcon } from '../../../ui-kit';
 
 function SecondBanner() {
   return (
-    <div className={styles.secondBanner__box}>
-      <h1 className={styles.title}>Четыре шага до нового сотрудника</h1>
-      <ul className={styles.cards}>
-        <li className={styles.card}>
-          <h2 className={styles.card__title}>1</h2>
-          <Text size="20px" weight="bold" color="grey80">
-            Заполните информацию о вакансии
-          </Text>
-        </li>
-        <li className={styles.card}>
-          <h2 className={styles.card__title}>2</h2>
-          <Text size="20px" weight="bold" color="grey80">
-            Выберите подходящие условия оплаты
-          </Text>
-        </li>
-        <li className={styles.card}>
-          <h2 className={styles.card__title}>3</h2>
-          <Text size="20px" weight="bold" color="grey80">
-            Укажите условия сотрудничества с рекрутером
-          </Text>
-        </li>
-        <li className={styles.card}>
-          <h2 className={styles.card__title}>4</h2>
-          <Text size="20px" weight="bold" color="grey80">
-            Оплатите размещение заявки
-          </Text>
-        </li>
-      </ul>
-      <div className={`${styles.icon} ${styles.icon__lilac}`}>
-        <StarIcon />
-      </div>
-      <div className={`${styles.icon} ${styles.icon__minty}`}>
-        <StarIcon color="minty" />
+    <div className={styles.secondBanner}>
+      <div className={styles.secondBanner__container}>
+        <h1 className={styles.title}>Четыре шага до нового сотрудника</h1>
+        <ul className={styles.cards}>
+          <li className={styles.card}>
+            <h2 className={styles.card__title}>1</h2>
+            <Text size="20px" weight="bold" color="grey80">
+              Заполните информацию о вакансии
+            </Text>
+          </li>
+          <li className={styles.card}>
+            <h2 className={styles.card__title}>2</h2>
+            <Text size="20px" weight="bold" color="grey80">
+              Выберите подходящие условия оплаты
+            </Text>
+          </li>
+          <li className={styles.card}>
+            <h2 className={styles.card__title}>3</h2>
+            <Text size="20px" weight="bold" color="grey80">
+              Укажите условия сотрудничества с рекрутером
+            </Text>
+          </li>
+          <li className={styles.card}>
+            <h2 className={styles.card__title}>4</h2>
+            <Text size="20px" weight="bold" color="grey80">
+              Оплатите размещение заявки
+            </Text>
+          </li>
+        </ul>
+        <div className={`${styles.icon} ${styles.icon__lilac}`}>
+          <StarIcon />
+        </div>
+        <div className={`${styles.icon} ${styles.icon__minty}`}>
+          <StarIcon color="minty" />
+        </div>
       </div>
     </div>
   );
